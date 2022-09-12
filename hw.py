127e2ce81c0bd95618fb0865773fa8f9ee500097
eisaeva <hanymka@me.com>
2022-09-12 15:04:18 +0300

    fix функции. Добавлен вывод аргументов функции

--- hw.py
+++ hw.py
@@ -1,18 +1,20 @@
 def print_func_name_and_args(func_name, *args):
     func_name = func_name.__name__.replace('_', ' ').capitalize()
-    print(f' наименование функции {func_name}')
+    print(f' Наименование функции: {func_name}')
+    for arg_name in args:
+        print(f' Значение аргументa: {arg_name}')
 
 
 def open_browser(browser_name):
-    print_func_name_and_args(open_browser)
+    print_func_name_and_args(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    print_func_name_and_args(go_to_companyname_homepage)
+    print_func_name_and_args(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    print_func_name_and_args(find_registration_button_on_login_page)
+    print_func_name_and_args(find_registration_button_on_login_page, page_url, button_text)
 
 
 open_browser('Chrome')
